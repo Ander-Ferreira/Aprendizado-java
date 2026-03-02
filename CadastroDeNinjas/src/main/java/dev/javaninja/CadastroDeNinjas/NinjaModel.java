7be488d6c0b1f7db93347ccb4ba087ae4cc4637f
Anderson Ferreira <andersonadm1314@gmail.com>
2026-03-02 18:18:16 -0300

    Usando o JPA para transformar minha classe NinhaModel em uma entidade de banco de dados

---
+++ CadastroDeNinjas/src/main/java/dev/javaninja/CadastroDeNinjas/NinjaModel.java
@@ -0,0 +1,50 @@
+package dev.javaninja.CadastroDeNinjas;
+
+import jakarta.persistence.*;
+
+
+@Entity //Aqui estou transformando esta classe em entidade para meu banco de dados
+@Table(name = "tb_cadastro") //Aqui estou colocando o nome da minha tabela
+public class NinjaModel {
+
+    @Id //irá gerar id no banco de dados
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //Estratégia para enumerar meus Id's na tabela (usa o auto increment do banco)
+    Long id;
+    String nome;
+    String email;
+    int idade;
+
+    //Meus construtores
+    public NinjaModel(){
+
+    }
+    public NinjaModel(String nome, String email, int idade){
+        this.nome = nome;
+        this.email = email;
+        this.idade = idade;
+    }
+
+    //Meus getters e setters
+    public String getNome(){
+        return nome;
+    }
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+
+    public String getEmail(){
+        return email;
+    }
+    public void setEmail(String email){
+        this.email = email;
+    }
+
+    public int getIdade(){
+        return idade;
+    }
+    public void setIdade(int idade){
+        this.idade = idade;
+    }
+
+
+}
