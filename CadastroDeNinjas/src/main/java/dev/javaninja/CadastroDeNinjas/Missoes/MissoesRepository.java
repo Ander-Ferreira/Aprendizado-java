ec9872d5ae4c4372fc6c7e5682dde23f91035263
Anderson Ferreira <andersonadm1314@gmail.com>
2026-05-15 04:56:58 -0300

    docs: adicionando explicacao sobre ORM

--- CadastroDeNinjas/src/main/java/dev/javaninja/CadastroDeNinjas/Missoes/MissoesRepository.java
+++ CadastroDeNinjas/src/main/java/dev/javaninja/CadastroDeNinjas/Missoes/MissoesRepository.java
@@ -10,5 +10,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 //
 // MissoesModel representa a entidade da tabela,
 // e Long representa o tipo da chave primária (ID).
+
+//ORM é uma tradução do nosso Model para o banco de dados, já que o banco de dados fala outra linguagem,
+//ele basicamente gera o esquema de tabelas no banco de dados.
+
 public interface MissoesRepository extends JpaRepository<MissoesModel, Long> {
 }
