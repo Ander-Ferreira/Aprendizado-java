5f9b39558052879b675b493b3df8afccbaa67b44
Anderson Ferreira <andersonadm1314@gmail.com>
2026-05-14 23:05:20 -0300

    Subindo notações Lombok

--- CadastroDeNinjas/src/main/java/dev/javaninja/CadastroDeNinjas/Missoes/MissoesModel.java
+++ CadastroDeNinjas/src/main/java/dev/javaninja/CadastroDeNinjas/Missoes/MissoesModel.java
@@ -2,11 +2,20 @@ package dev.javaninja.CadastroDeNinjas.Missoes;
 
 import dev.javaninja.CadastroDeNinjas.Ninjas.NinjaModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Entity
 @Table(name = "tb_missoes")
+
+//Notações Lombok
+@NoArgsConstructor //Cria um construtor sem argumentos por baixo dos panos
+@AllArgsConstructor //Cria um construtor com todos os argumentos por baixo dos pontos, e atualiza com novos atributos
+@Data //Cria  automaticamente meus getters e setters por baixo dos panos
+
 public class MissoesModel {
    @Id
    @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -19,32 +28,7 @@ public class MissoesModel {
    @OneToMany(mappedBy = "missoes") //Uma missão para muitos ninjas
    private List<NinjaModel> ninjas;
 
-   //Meus construtores
-    public MissoesModel(){
 
-    }
-
-    //Meus getters e setters
-    public Long getId(){
-        return id;
-    }
-    public void setId(Long id){
-        this.id = id;
-    }
-
-    public String getNome(){
-        return nome;
-    }
-    public void setNome(String nome){
-        this.nome = nome;
-    }
-
-    public String getDificuldade(){
-        return dificuldade;
-    }
-    public void setDificuldade(String dificuldade){
-        this.dificuldade = dificuldade;
-    }
 
 
 
--- CadastroDeNinjas/src/main/java/dev/javaninja/CadastroDeNinjas/Ninjas/NinjaModel.java
+++ CadastroDeNinjas/src/main/java/dev/javaninja/CadastroDeNinjas/Ninjas/NinjaModel.java
@@ -2,14 +2,22 @@ package dev.javaninja.CadastroDeNinjas.Ninjas;
 
 import dev.javaninja.CadastroDeNinjas.Missoes.MissoesModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 
 @Entity //Aqui estou transformando esta classe em entidade para meu banco de dados
 @Table(name = "tb_cadastro") //Aqui estou colocando o nome da minha tabela
-public class NinjaModel {
 
+//Notações Lombok
+@NoArgsConstructor //Cria um construtor sem argumentos por baixo dos panos
+@AllArgsConstructor //Cria um construtor com todos os argumentos por baixo dos pontos, e atualiza com novos atributos
+@Data //Cria  automaticamente meus getters e setters por baixo dos panos
+
+public class NinjaModel {
     @Id //irá gerar id no banco de dados
     @GeneratedValue(strategy = GenerationType.IDENTITY) //Estratégia para enumerar meus Id's na tabela (usa o auto increment do banco)
 
@@ -25,37 +33,6 @@ public class NinjaModel {
    @JoinColumn(name = "missoes_id") //Fusão da tabela de missões com a de ninja
    private MissoesModel missoes;
 
-    //Meus construtores
-    public NinjaModel(){
-
-    }
-    public NinjaModel(String nome, String email, int idade){
-        this.nome = nome;
-        this.email = email;
-        this.idade = idade;
-    }
-
-    //Meus getters e setters
-    public String getNome(){
-        return nome;
-    }
-    public void setNome(String nome){
-        this.nome = nome;
-    }
-
-    public String getEmail(){
-        return email;
-    }
-    public void setEmail(String email){
-        this.email = email;
-    }
-
-    public int getIdade(){
-        return idade;
-    }
-    public void setIdade(int idade){
-        this.idade = idade;
-    }
 
 
 
