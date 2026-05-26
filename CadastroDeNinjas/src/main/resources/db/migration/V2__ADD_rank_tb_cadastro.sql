8ec0a2135745318726f9f328ff33eaa21ce905eb
Anderson Ferreira <andersonadm1314@gmail.com>
2026-05-26 16:36:31 -0300

    chore: adicionando directorio de versionamento de banco de dados ao gitignore

--- CadastroDeNinjas/src/main/resources/db/migration/V2__ADD_rank_tb_cadastro.sql
+++
@@ -1,7 +0,0 @@
---Exemplo de versionamento de banco de dados com o flyway
---Este arquivo é imútavel, para adicionar uma nova tabela e criar uma nova versão do banco de dados
---Deve-se criar um novo arquivo, exemplo: V3__ADD__NOVA_COLUNA_TB_CADASTRO.sql
-
-
-ALTER TABLE tb_cadastro
-ADD COLUMN rank VARCHAR(255);
