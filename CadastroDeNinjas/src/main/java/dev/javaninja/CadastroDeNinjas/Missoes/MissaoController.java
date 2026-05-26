7e72f4a9d2927c74c48188e5b4d2415d29b0cac8
Anderson Ferreira <andersonadm1314@gmail.com>
2026-05-26 16:37:12 -0300

    feat: adicionando rotas aos controllers de Ninja e Missao

--- CadastroDeNinjas/src/main/java/dev/javaninja/CadastroDeNinjas/Missoes/MissaoController.java
+++ CadastroDeNinjas/src/main/java/dev/javaninja/CadastroDeNinjas/Missoes/MissaoController.java
@@ -1,4 +1,35 @@
 package dev.javaninja.CadastroDeNinjas.Missoes;
 
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("missoes") // vai redirecionar minhas requisições automaticamente para http://localhost/missoes/requisicoes
 public class MissaoController {
+
+    @GetMapping("/buscarmissoes")
+    public String buscarMissoes(){
+        return "todas missões";
+    }
+
+    @GetMapping("/buscarid")
+    public String buscarMissaoPorId(){
+        return "missao por id";
+    }
+
+    @PostMapping("/cadastrar")
+    public String cadastrarMissao(){
+        return "missao cadastrada";
+    }
+
+    @PutMapping("/atualizar")
+    public String atualizarMissao(){
+        return "Missao atualizada";
+    }
+
+    @DeleteMapping
+    public String deletarMissao(){
+        return "Missao deletada com sucesso";
+    }
+
+
 }
--- CadastroDeNinjas/src/main/java/dev/javaninja/CadastroDeNinjas/Ninjas/NinjaController.java
+++ CadastroDeNinjas/src/main/java/dev/javaninja/CadastroDeNinjas/Ninjas/NinjaController.java
@@ -1,16 +1,39 @@
 package dev.javaninja.CadastroDeNinjas.Ninjas;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController //Controlador para APIrest -> aqui falo para o java que isto é um controller
 @RequestMapping //Aqui estou mapeando minhas rotas
 public class NinjaController {
 
-    //Meu método de boas vindas
-    @GetMapping("/boasvindas") // Aqui coloco a rota que quero mostrar minhas boas vindas
-    public String boasVindas(){
-        return "Bem-vindo a minha aplicação";
+    //Criar Ninja (CREATE)
+    @PostMapping("/cadastrar")
+    public String criarNinja(){
+        return "Ninja criado!";
     }
+
+    //Mostrar todos os ninjas (READ)
+    @GetMapping("/todos")
+    public String buscarTodosNinjas(){
+        return "todos os ninjas!";
+    }
+
+    //Mostrar Ninja por Id (READ)
+    @GetMapping("/buscarid")
+    public String mostrarNinjaPorId(){
+        return "ninja por id";
+    }
+
+    //Atualizar Ninja (UPDATE)
+    @PutMapping("/atualizar")
+    public String atualizarNinja(){
+        return "Ninja atualizado";
+    }
+
+    //Deletar Ninja (DELETE)
+    @DeleteMapping("/deletar")
+    public String deletarNinja(){
+        return "ninja deletado";
+    }
+
 }
