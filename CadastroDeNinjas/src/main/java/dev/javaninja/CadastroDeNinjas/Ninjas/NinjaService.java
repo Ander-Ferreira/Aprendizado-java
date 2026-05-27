aeca0572ff1d702268a8948d9c70dd4dd926cd29
Anderson Ferreira <andersonadm1314@gmail.com>
2026-05-26 22:03:19 -0300

    afeat: criando service com o primeiro metodo de buscar todos

--- CadastroDeNinjas/src/main/java/dev/javaninja/CadastroDeNinjas/Ninjas/NinjaService.java
+++ CadastroDeNinjas/src/main/java/dev/javaninja/CadastroDeNinjas/Ninjas/NinjaService.java
@@ -1,4 +1,22 @@
 package dev.javaninja.CadastroDeNinjas.Ninjas;
 
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class NinjaService {
+
+    //Injeção de dependencia para trazer o objeto NinjaRepository com os métodos do JPA
+    private NinjaRepository ninjaRepository;
+    public NinjaService (NinjaRepository ninjaRepository){
+        this.ninjaRepository = ninjaRepository;
+    }
+
+    //Método para listar todos os meus ninjas
+    public List<NinjaModel> listarNinjas(){
+        return ninjaRepository.findAll();
+
+    }
+
 }
