8626219bc8c231e328186b4b6720abf88bc56a27
Anderson Ferreira <andersonadm1314@gmail.com>
2026-05-29 00:26:56 -0300

    chore: inclui a versão do DTO da entidade NinjaModel e mapeia manualmente o NinjaMapper com a proteção DTO

--- CadastroDeNinjas/src/main/java/dev/javaninja/CadastroDeNinjas/Ninjas/NinjaDTO.java
+++ CadastroDeNinjas/src/main/java/dev/javaninja/CadastroDeNinjas/Ninjas/NinjaDTO.java
@@ -1,4 +1,21 @@
 package dev.javaninja.CadastroDeNinjas.Ninjas;
 
+import dev.javaninja.CadastroDeNinjas.Missoes.MissoesModel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+//O DTO serve para tirar a responsabilidade da minha entidade NinjaModel, todos atributos que NinjaModel tem, o DTO também vai ter
+//Essa camada de abstração ajuda na defesa do meu banco de dados
 public class NinjaDTO {
+    private Long id;
+    private String nome;
+    private String email;
+    private int idade;
+    private String imgUrl;
+    private String rank;
+    private MissoesModel missoes;
 }
--- CadastroDeNinjas/src/main/java/dev/javaninja/CadastroDeNinjas/Ninjas/NinjaMapper.java
+++ CadastroDeNinjas/src/main/java/dev/javaninja/CadastroDeNinjas/Ninjas/NinjaMapper.java
@@ -1,4 +1,42 @@
 package dev.javaninja.CadastroDeNinjas.Ninjas;
 
+import org.springframework.stereotype.Component;
+
+//O mapper é o que conecta minha Classe ao meu DTO
+@Component
 public class NinjaMapper {
+
+    public NinjaModel map(NinjaDTO ninjaDTO){
+        //Inicializo um objeto do tipo NinjaModel
+        NinjaModel ninjaModel = new NinjaModel();
+        //Agora coloco os atributos do meu ninjaDTO dentro do meu ninjaModel
+        ninjaModel.setId(ninjaDTO.getId());
+        ninjaModel.setNome(ninjaDTO.getNome());
+        ninjaModel.setEmail(ninjaDTO.getEmail());
+        ninjaModel.setIdade(ninjaDTO.getIdade());
+        ninjaModel.setImgUrl(ninjaDTO.getImgUrl());
+        ninjaModel.setRank(ninjaDTO.getRank());
+        ninjaModel.setMissoes(ninjaDTO.getMissoes());
+
+        //Retorno o ninjaModel com os atributos do DTO
+        return ninjaModel;
+    }
+
+    //Agora estou colocando os atributos do meu NinjaModel dentro do meu DTO
+    public NinjaDTO map(NinjaModel ninjaModel){
+        NinjaDTO ninjaDTO = new NinjaDTO();
+        ninjaDTO.setId(ninjaModel.getId());
+        ninjaDTO.setNome(ninjaModel.getNome());
+        ninjaDTO.setIdade(ninjaModel.getIdade());
+        ninjaDTO.setEmail(ninjaModel.getEmail());
+        ninjaDTO.setImgUrl(ninjaModel.getImgUrl());
+        ninjaDTO.setRank(ninjaModel.getRank());
+        ninjaDTO.setMissoes(ninjaModel.getMissoes());
+
+        //Retorno o DTO com os atributos do NinjaModel
+        return ninjaDTO;
+
+    }
+
+
 }
