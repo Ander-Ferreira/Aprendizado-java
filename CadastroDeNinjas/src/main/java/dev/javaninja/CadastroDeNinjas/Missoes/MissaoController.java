8a3109fd981466a31b45973b7a2a9624328ad830
Anderson Ferreira <andersonadm1314@gmail.com>
2026-06-23 20:03:02 -0300

    docs: documentando a rota de missões

--- CadastroDeNinjas/src/main/java/dev/javaninja/CadastroDeNinjas/Missoes/MissaoController.java
+++ CadastroDeNinjas/src/main/java/dev/javaninja/CadastroDeNinjas/Missoes/MissaoController.java
@@ -1,5 +1,8 @@
 package dev.javaninja.CadastroDeNinjas.Missoes;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -8,6 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("missoes") // vai redirecionar minhas requisições automaticamente para http://localhost:8080/missoes/
+@Tag(name = "missoes", description = "Rota de missões") //Documentação para o swagger
 public class MissaoController {
 
     //Injeção de dependência do meu MissoesService
@@ -17,6 +21,13 @@ public class MissaoController {
     }
 
     @GetMapping("/buscar")
+
+    // ------ Documentação para o swagger -------
+    @Operation(summary = "Busca todas as missões",
+            description = "Retorna a lista de todas as missões cadastradas")
+    @ApiResponse(responseCode = "200", description = "Missões listadas com sucesso")
+    //-------------------------------------------
+
     //ResposeEntity me permiti retornar respostar http
     //O tipo será uma lista porque estarei recebendo e retornando uma lista de DTO
     public ResponseEntity<List<MissoesDTO>> buscarMissoes(){
@@ -25,6 +36,14 @@ public class MissaoController {
     }
 
     @GetMapping("/buscar/{id}")
+
+    // ------ Documentação para o swagger -------
+    @Operation(summary = "Buscar missão por id",
+            description = "Busca a missão por id, se encontrada retorna a missão, caso não exista (seja null) retorna uma mensagem 404\"")
+    @ApiResponse(responseCode = "200", description = "Missão encontrada")
+    @ApiResponse(responseCode = "404", description = "Missão não encontrada")
+    //-------------------------------------------
+
     //O tipo será um generic porque precisarei retornar tipos diferentes, DTO para caso ache,
     // mensagem String para caso não ache
     public ResponseEntity<?> buscarMissaoPorId(@PathVariable Long id){
@@ -37,12 +56,26 @@ public class MissaoController {
 
 
     @PostMapping("/criar")
+
+    // ------ Documentação para o swagger -------
+    @Operation(summary = "Cadastro de missão", description = "Cadastro completo de missão")
+    @ApiResponse(responseCode = "201", description = "Missão criada com sucesso")
+    //-------------------------------------------
+
     public ResponseEntity<String> cadastrarMissao(@RequestBody MissoesDTO missoesDTO){
         missoesService.cadastrarMissao(missoesDTO);
         return ResponseEntity.status(HttpStatus.CREATED).body("Missão criada com sucesso!");
     }
 
     @PutMapping("/atualizar/{id}")
+
+    // ------ Documentação para o swagger -------
+    @Operation(summary = "Atualiza missão por id",
+            description = "Busca a missão por id, se encontrada retorna a missão, caso não exista (seja null) retorna uma mensagem 404")
+    @ApiResponse(responseCode = "200", description = "Missão alterada com sucesso")
+    @ApiResponse(responseCode = "404", description = "Missão não encontrada")
+    //-------------------------------------------
+
     public ResponseEntity<?> atualizarMissao(@PathVariable Long id, @RequestBody MissoesDTO missoesDTO){
        MissoesDTO missaoExiste = missoesService.buscarPorId(id);
        if(missaoExiste != null){
@@ -53,6 +86,14 @@ public class MissaoController {
     }
 
     @DeleteMapping("deletar/{id}")
+
+    // ------ Documentação para o swagger -------
+    @Operation(summary = "Deleta missão por id",
+            description = "Busca a missão por id, se encontrada exclui-se a missão, caso não exista (seja null) retorna uma mensagem 404")
+    @ApiResponse(responseCode = "200", description = "Missão excluída com sucesso")
+    @ApiResponse(responseCode = "404", description = "Missão não encontrada")
+    //-------------------------------------------
+
     public ResponseEntity<String> deletarMissao(@PathVariable Long id){
         //Faço uma busca
         MissoesDTO missaoExiste = missoesService.buscarPorId(id);
