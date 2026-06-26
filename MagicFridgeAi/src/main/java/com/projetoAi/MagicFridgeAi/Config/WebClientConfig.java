307123649d111698cd00ff7be89c5c397a9bbf32
Anderson Ferreira <andersonadm1314@gmail.com>
2026-06-25 21:38:20 -0300

    build: commit inicial, configuração do banco de dados e instalações de dependencias

---
+++ MagicFridgeAi/src/main/java/com/projetoAi/MagicFridgeAi/Config/WebClientConfig.java
@@ -0,0 +1,17 @@
+package com.projetoAi.MagicFridgeAi.Config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+public class WebClientConfig {
+    @Value("${API_URL}")
+    String chatGptUrl;
+
+    @Bean
+    public WebClient webClient(WebClient.Builder builder){
+          return builder.baseUrl(chatGptUrl).build();
+    }
+}
